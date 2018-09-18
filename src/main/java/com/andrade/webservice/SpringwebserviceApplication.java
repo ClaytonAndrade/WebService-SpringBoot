3a1487d158f2fb23da1115f829bc777756d583f4
Clayton Andrade <clayton_m_andrade@hotmail.com>
2018-09-18 15:10:56 -0300

    Inicialização do Projeto

---
+++ src/main/java/com/andrade/webservice/SpringwebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.andrade.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebserviceApplication.class, args);
+	}
+}
